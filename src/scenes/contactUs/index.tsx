203279dc8919ae69ad0210609f017d064b2d1970
Ekeski <ekemewomaogbofavour@gmail.com>
2026-02-27 20:38:40 +0000

    Completed Our classes section

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,81 @@
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import { useForm } from "react-hook-form";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+
+    const {
+        register,
+        trigger,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = async (e: any) => {
+        const isValid = await trigger();
+        if (!isValid) {
+            e.preventDefault();
+        }
+    }
+
+  return (
+    <section id="contactUs" className="mx-auto w-5/6 pt-24 pb-32">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.ContactUs)}
+      >
+        {/* HEADER */}
+        <motion.div
+          className="md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HText>
+            <span className="text-primary-500">JOIN NOW</span>
+            TO GET IN SHAPE
+          </HText>
+          <p className="my-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            officia tempora dolore! Aliquid pariatur, omnis quibusdam tenetur
+            odit eveniet rem illo! Iusto temporibus sit rerum architecto et
+            esse! Quo, natus?
+          </p>
+        </motion.div>
+
+        {/* FORM AND IMAGE */}
+        <div className="mt-10 justify-between gap-8 md:flex">
+          <motion.div
+            className="mt-10 basis-3/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <form
+                target="_blank"
+                onSubmit={onSubmit}
+            >
+
+            </form>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default ContactUs;
