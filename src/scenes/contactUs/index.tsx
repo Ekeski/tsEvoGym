5f2f64d5128c540791c8433ef8389a00a1655133
Ekeski <ekemewomaogbofavour@gmail.com>
2026-02-28 16:25:38 +0000

    Completed the project

--- src/scenes/contactUs/index.tsx
+++ src/scenes/contactUs/index.tsx
@@ -9,19 +9,20 @@ type Props = {
 };
 
 const ContactUs = ({ setSelectedPage }: Props) => {
+  const inputStyles = `mb-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white`;
 
-    const {
-        register,
-        trigger,
-        formState: { errors }
-    } = useForm();
+  const {
+    register,
+    trigger,
+    formState: { errors },
+  } = useForm();
 
-    const onSubmit = async (e: any) => {
-        const isValid = await trigger();
-        if (!isValid) {
-            e.preventDefault();
-        }
+  const onSubmit = async (e: any) => {
+    const isValid = await trigger();
+    if (!isValid) {
+      e.preventDefault();
     }
+  };
 
   return (
     <section id="contactUs" className="mx-auto w-5/6 pt-24 pb-32">
@@ -66,11 +67,94 @@ const ContactUs = ({ setSelectedPage }: Props) => {
             }}
           >
             <form
-                target="_blank"
-                onSubmit={onSubmit}
+              target="_blank"
+              onSubmit={onSubmit}
+              action="https://formsubmit.co/c2436088026baa9f79951c1c6d05f58"
+              method="POST"
             >
+              {/* NAME INPUT */}
+              <input
+                type="text"
+                className={inputStyles}
+                placeholder="NAME"
+                {...register("name", {
+                  required: true,
+                  maxLength: 100,
+                })}
+              />
+              {errors.name && (
+                <p className="mt-1 text-primary-500">
+                  {errors.name.type === "required" && "This field is required."}
+                  {errors.name.type === "maxLength" &&
+                    "Max length is 100 characters."}
+                </p>
+              )}
 
+              {/* EMAIL INPUT */}
+              <input
+                type="text"
+                className={inputStyles}
+                placeholder="EMAIL"
+                {...register("email", {
+                  required: true,
+                  pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                })}
+              />
+              {errors.email && (
+                <p className="mt-1 text-primary-500">
+                  {errors.email.type === "required" &&
+                    "This field is required."}
+                  {errors.email.type === "pattern" && "Invalid email address."}
+                </p>
+              )}
+
+              {/* MESSAGE INPUT */}
+              <textarea
+                className={inputStyles}
+                rows={4}
+                cols={50}
+                placeholder="Message"
+                {...register("message", {
+                  required: true,
+                  maxLength: 2000,
+                })}
+              />
+              {errors.message && (
+                <p className="mt-1 text-primary-500">
+                  {errors.message.type === "required" &&
+                    "This field is required."}
+                  {errors.message.type === "maxLength" &&
+                    "Max length is 2000 characters."}
+                </p>
+              )}
+
+              <button
+                type="submit"
+                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
+              >
+                SUBMIT
+              </button>
             </form>
+          </motion.div>
+
+          <motion.div
+            className="relative mt-16 basis-2/5 md:mt-0"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            variants={{
+              hidden: { opacity: 0, y: 50 },
+              visible: { opacity: 1, y: 0 },
+            }}
+          >
+            <div className="md:before:content-evolvetext w-full before:absolute before:-right-10 before:top-[420px] before:z-[-1]">
+              <img
+                src={ContactUsPageGraphic}
+                alt="contact-us-page-graphic"
+                className="w-full"
+              />
+            </div>
           </motion.div>
         </div>
       </motion.div>
